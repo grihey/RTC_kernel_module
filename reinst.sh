70010977ea967b704d161c8535255f9a82fd9af0
Grigoriy Romanov <grihey138@gmail.com>
2016-08-02 03:40:02 +0300

    Chardev registering, allow to exec mknod util for dev file appearing in /dev.

--- reinst.sh
+++ reinst.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+rm /dev/rtc38
 rmmod -f rtc_romanov.ko
 insmod rtc_romanov.ko
+mknod /dev/rtc38 c 250 0
--- rtc_romanov.c
+++ rtc_romanov.c
@@ -11,12 +11,13 @@
 
 #include <asm/uaccess.h>
 #include <linux/fs.h>
+#include <linux/proc_fs.h>
 #include <linux/slab.h>
 #include <linux/kthread.h>
 #include <linux/random.h>
 
 #define DEVICE_NAME "rtc38"
-#define CLASS_NAME "rtc"
+#define procfs_name "helloworld"
 
 #define MIL 1000000L
 #define S_FACTOR 100
@@ -26,19 +27,44 @@
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("Grigoriy Romanov");
 
-static long   epoch_time_sec = 0;
-static int    epoch_time_usec = 0;
+static long   time_sec = 0;
+static int    time_usec = 0;
 static int    speed = 100;
 static int    random = 0;
 static short  random_bound = 300;
 
-module_param(epoch_time_sec, long, S_IRUGO | S_IWUGO);
-module_param(epoch_time_usec, int, S_IRUGO | S_IWUGO);
+module_param(time_sec, long, S_IRUGO | S_IWUGO);
+module_param(time_usec, int, S_IRUGO | S_IWUGO);
 module_param(speed, int, S_IRUGO | S_IWUGO);
 module_param(random, int, S_IRUGO | S_IWUGO);
 module_param(random_bound, short, S_IRUGO | S_IWUGO);
 
 static int main_thread(void* data);
+static int rtc_romanov_init(void);
+void rtc_romanov_exit(void);
+
+static int device_open(struct inode *, struct file *);
+static int device_release(struct inode *, struct file *);
+static ssize_t device_read(struct file *, char *, size_t, loff_t *);
+static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
+
+#define SUCCESS 0
+#define DEVICE_NAME "rtc38"
+#define BUF_LEN 80
+
+static int Major;
+static int Device_Open = 0;
+static char msg[BUF_LEN];
+static char *msg_Ptr;
+
+struct proc_dir_entry *proc_file;
+
+static struct file_operations fops = {
+  .read = device_read,
+  .write = device_write,
+  .open = device_open,
+  .release = device_release
+};
 
 struct task_struct *task;
 
@@ -49,19 +75,36 @@ static int rtc_romanov_init(void)
   do_gettimeofday(tv);
 
   /*Init time parameters, which store seconds and useconds*/
-  epoch_time_sec = tv->tv_sec;
-  epoch_time_usec = tv->tv_usec;
+  time_sec = tv->tv_sec;
+  time_usec = tv->tv_usec;
 
   task = kthread_run(main_thread, &tv, "main loop");
   wake_up_process(task);
 
-  printk(KERN_ALERT "RTC_Romanov module have been istalled. \n");
+  Major = register_chrdev(0, DEVICE_NAME, &fops);
 
-  return 0;
+  if(Major < 0) {
+    printk(KERN_ALERT "Registering chardevice failed with %d\n", Major); 
+    return Major;
+  }
+  printk(KERN_INFO "I was assigned major number %d. To talk to \n", Major);
+  printk(KERN_INFO "the driver, create a dev file with\n");
+  printk(KERN_INFO "'mknod /dev/%s c %d 0'.\n", DEVICE_NAME, Major);
+  printk(KERN_INFO "the device file.\n");
+  printk(KERN_INFO "Remove the device file and module when done.\n");
+
+  return SUCCESS;
 }
 
 void rtc_romanov_exit(void)
 {
+  unregister_chrdev(Major, DEVICE_NAME);
+
+  /*
+  if(ret < 0)
+    printk(KERN_ALERT "Error in unregistere_chrdev: %d\n", ret);
+  */
+
   kthread_stop(task);
   printk(KERN_ALERT "RTC_Romanov module was removed.\n");
 }
@@ -81,22 +124,65 @@ static int main_thread(void* data)
       get_random_bytes(&rand_speed, sizeof(rand_speed));
       rand_speed %= random_bound;
 
-      epoch_time_sec += (S_QUOT * rand_speed +
-          S_REMAINDER * rand_speed + epoch_time_usec) / MIL;
-      epoch_time_usec = (S_QUOT * rand_speed + 
-          S_REMAINDER * rand_speed + epoch_time_usec) % MIL;
+      time_sec += (S_QUOT * rand_speed +
+          S_REMAINDER * rand_speed + time_usec) / MIL;
+      time_usec = (S_QUOT * rand_speed + 
+          S_REMAINDER * rand_speed + time_usec) % MIL;
       
     } else {
-      epoch_time_sec += (S_QUOT * speed +
-          S_REMAINDER * speed + epoch_time_usec) / MIL;
-      epoch_time_usec = (S_QUOT * speed + 
-          S_REMAINDER * speed + epoch_time_usec) % MIL;
+      time_sec += (S_QUOT * speed +
+          S_REMAINDER * speed + time_usec) / MIL;
+      time_usec = (S_QUOT * speed + 
+          S_REMAINDER * speed + time_usec) % MIL;
     }
-
-    printk("After increment secs %ld, usecs %d\n", 
-        epoch_time_sec, epoch_time_usec);
   }
     return 0;
+}
+
+static int device_open(struct inode *inode, struct file *file)
+{
+  if(Device_Open)
+    return -EBUSY;
+  Device_Open++;
+  sprintf(msg, "Seconds: %ld; Useconds: %d.\n",
+      time_sec, time_usec);
+  msg_Ptr = msg;
+  try_module_get(THIS_MODULE);
+
+  return SUCCESS;
+}
+
+static int device_release(struct inode *inode, struct file *file)
+{
+  Device_Open--;
+  module_put(THIS_MODULE);
+  return 0;
+}
+
+static ssize_t device_read(struct file *filp,
+                           char *buffer,
+                           size_t length,
+                           loff_t * offset)
+{
+  int bytes_read = 0;
+  if(*msg_Ptr == 0)
+    return 0;
+
+  while(length && *msg_Ptr){
+    put_user(*(msg_Ptr++), buffer++);
+    length--;
+    bytes_read++;
+  }
+  return bytes_read;
+}
+
+static ssize_t device_write(struct file *filp, 
+                            const char *buffer, 
+                            size_t length, 
+                            loff_t * offset)
+{
+  printk(KERN_ALERT "Write not implementing on RTC Romanov module.\n");
+  return -EINVAL;
 }
 
 module_init(rtc_romanov_init);
